e4a102177dc0162a3a38585f42244a65db2c2c55
Michael Frieze <michaelfrieze@icloud.com>
2022-06-02 22:27:28 -0400

    updated BlogPostCard component with links to blog post slug and setup for fly deployment

--- app/components/blog-post-card.jsx
+++ app/components/blog-post-card.jsx
@@ -3,7 +3,7 @@ import { Link } from '@remix-run/react';
 export const BlogPostCard = ({ blogPost }) => {
   return (
     <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-      <div className="flex-shrink-0">
+      <Link to={`/blog/${blogPost.attributes.slug}`} className="flex-shrink-0">
         {blogPost.attributes.image?.data?.[0].attributes?.formats?.medium
           ?.url && (
           <img
@@ -14,15 +14,10 @@ export const BlogPostCard = ({ blogPost }) => {
             alt=""
           />
         )}
-      </div>
+      </Link>
       <div className="flex-1 bg-white p-6 flex flex-col justify-between">
         <div className="flex-1">
-          <p className="text-sm font-medium text-indigo-600">
-            <Link to="/blog/dynamicroute" className="hover:underline">
-              Article
-            </Link>
-          </p>
-          <Link to="/blog/dynamicroute" className="block mt-2">
+          <Link to={`/blog/${blogPost.attributes.slug}`} className="block mt-2">
             <p className="text-xl font-semibold text-gray-900">
               {blogPost.attributes.title}
             </p>

230feed1896526597f7658e157102fdf415887fc
Michael Frieze <michaelfrieze@icloud.com>
2022-06-11 18:10:57 -0400

    finished header for desktop view

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,4 +1,11 @@
 import { Link } from '@remix-run/react';
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedin,
+} from 'react-icons/fa';
+import { BsMedium } from 'react-icons/bs';
 import headerStyles from '~/styles/header.css';
 
 export const links = () => [
@@ -13,21 +20,39 @@ export function Header() {
     <header className="header-container">
       <Link to="/" prefetch="intent" className="logo">
         <img
-          src="/images/logos/logo-header.webp"
-          alt="logo"
+          srcSet="/images/logos/logo-header.webp"
+          alt="Logo"
           width={376}
           height={87}
         />
       </Link>
-      <div className="social-button"></div>
-      <nav className="nav-links">
-        <a href="/">Mission</a>
-        <a href="/">About Us</a>
-        <Link to="/">Blog</Link>
-        <a href="/">Team</a>
-        <a href="/">Careers</a>
-        <a href="/">Contact Us</a>
-      </nav>
+      <div>
+        <div className="social-btn-container">
+          <a className="social-btn" target="_blank" href="/">
+            <FaFacebookF />
+          </a>
+          <a className="social-btn" target="_blank" href="/">
+            <FaTwitter />
+          </a>
+          <a className="social-btn" target="_blank" href="/">
+            <FaInstagram />
+          </a>
+          <a className="social-btn" target="_blank" href="/">
+            <BsMedium />
+          </a>
+          <a className="social-btn" target="_blank" href="/">
+            <FaLinkedin />
+          </a>
+        </div>
+        <nav className="nav-links">
+          <a href="/">Mission</a>
+          <a href="/">About Us</a>
+          <Link to="/">Blog</Link>
+          <a href="/">Team</a>
+          <a href="/">Careers</a>
+          <a href="/">Contact Us</a>
+        </nav>
+      </div>
     </header>
   );
 }

2546734da7539d61d8f94d021d62ed16a87e1159
Michael Frieze <michaelfrieze@icloud.com>
2022-06-15 19:58:46 -0400

    updatead hero section with new figma design

--- app/components/values.jsx
+++ app/components/values.jsx
@@ -13,14 +13,6 @@ export function Values() {
     <section className="values-section">
       <div className="values-container">
         <div className="values-heading">
-          <img
-            className="values-butterfly-image"
-            srcSet="/images/graphics/values-butterfly.webp"
-            alt="butterfly"
-            width={266}
-            height={244}
-          />
-
           <div className="values-text-box">
             <h3 className="values-section-title">OUR VALUES</h3>
             <h1 className="values-header-text">

d2ff30963441e4f72682d37a68ae13c7c7c35689
Michael Frieze <michaelfrieze@icloud.com>
2022-06-26 16:07:36 -0400

    finished news-index-heading-section in the NewsIndexRoute

--- app/routes/news/index.jsx
+++ app/routes/news/index.jsx
@@ -14,7 +14,26 @@ export default function NewsIndexRoute() {
   return (
     <div className="layout-container">
       <Header />
-      <main></main>
+      <main>
+        <section className="news-index-heading-section">
+          <div className="news-index-heading-container">
+            <h1 className="news-index-heading">
+              Pixie Meta{' '}
+              <span className="news-index-heading-highlight">News</span>
+            </h1>
+            <div className="news-index-subheading-container">
+              <label htmlFor="news-posts-filter">FILTER BY: </label>
+              <select
+                className="news-index-subheading-filter-dropdown"
+                id="news-posts-filter"
+                name="news-posts-filter"
+              >
+                <option value="">NFT’S</option>
+              </select>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
